2c9e38ff13f55ffd2a5b0c7f158690c27df05b82
wepapepa <catu.leibrin@gmail.com>
2024-05-28 22:51:10 -0400

    fixed main file

--- server.js
+++ server.js
@@ -8,3 +8,19 @@ const productManager = newProductsManager(`${__dirname}/db/products.json`)
 
 const app = express()
 
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api/carts', cartRouter)
+app.use('/api/products', productsRouter)
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'handlebars')
+
+app.use('/', viewsRouter)
+
+const PORT = 8080;
+
+const httpServer = app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
